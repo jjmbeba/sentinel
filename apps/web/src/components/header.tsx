de7a19390aec2f4e7fd402b23af3c01e08813a99
John Johnson <jjmbeba@gmail.com>
2025-07-16 21:23:32 +0300

    trim header nav links

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,16 +1,10 @@
-import { Link, linkOptions, useRouterState } from "@tanstack/react-router";
+import { Link, useRouterState } from "@tanstack/react-router";
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "./mode-toggle";
 import { buttonVariants } from "./ui/button";
 import UserMenu from "./user-menu";
 
 export default function Header() {
-	const links = linkOptions([
-		{ to: "/", label: "Home" },
-		{ to: "/dashboard", label: "Dashboard" },
-		{ to: "/todos", label: "Todos" },
-	]);
-
 	const pathname = useRouterState({
 		select: (s) => s.location.pathname,
 	});
@@ -30,26 +24,21 @@ export default function Header() {
 		<div>
 			<div className="flex flex-row items-center justify-between px-6 py-3">
 				<nav className="flex gap-4 text-md">
-					{links.map(({ to, label }) => {
-						return (
-							<Link
-								activeProps={{
-									className: "underline",
-								}}
-								className={cn(
-									buttonVariants({
-										variant: "link",
-										size: "sm",
-										effect: "hoverUnderline",
-									})
-								)}
-								key={to}
-								to={to}
-							>
-								{label}
-							</Link>
-						);
-					})}
+					<Link
+						activeProps={{
+							className: "underline",
+						}}
+						className={cn(
+							buttonVariants({
+								variant: "link",
+								size: "sm",
+								effect: "hoverUnderline",
+							})
+						)}
+						to="/"
+					>
+						Home
+					</Link>
 				</nav>
 				<div className="flex items-center gap-2">
 					<ModeToggle />
